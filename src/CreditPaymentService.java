6e148444f517f5712b2081fe963241623707cfa0
YukhnikOlga <olga.iukhnik@gmail.com>
2024-05-26 23:16:09 +0300

    find deffect

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,20 +1,23 @@
 public class CreditPaymentService {
-    public int calculate(int amountCredit, int month, double percentYear) {
-        // X = S * (PM * (1 + PM)^n)) / ((1 + PM)^n ) -1)
+    public int calculate(double percentYear, int amountCredit, int months) {
+        // X = S * (PM * (1 + PM)^n)) / ((1 + PM)^n -1)
         // a = (1 + PM)
         // b = (1 + PM)^n
-        // c = (PM * (1 + PM)^n)) / ((1 + PM)^n ) -1);
+        // c = (PM * (1 + PM)^n)) / ((1 + PM)^n -1);
         // Х - размер ежемесячного платежа      -amountPayment
         // S — сумма займа                      -amountCredit
         // P — процентная ставка годовая        -percentYear
         // PM - процентная ставка месячная      -percentMonth
         // n — срок кредитования (в месяцах)    -month
 
-        double percentMonth = percentYear / month / 100;
+        double percentMonth = percentYear / 12 / 100;
         double a = 1 + percentMonth;
-        double b = Math.pow(a, month); // a^n
-        double c = (percentMonth * b) / (b - 1);
-        double amountPayment = amountCredit * c;
+        double b = Math.pow(a, months); // a^n
+        double c = percentMonth * b;
+        double d = b - 1;
+        double e = c / d; // коэффициент аннулитета
+        double amountPayment = amountCredit * e;
+
         return (int) amountPayment;
 
     }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int amountCredit = 1_000_000; // сумма кредита
+
         double percentYear = 9.99; // процентная ставка в год
-        int month = 12;
+        int amountCredit = 1_000_000; // сумма кредита
+        int months = 12;
 
-       // for (int month = 12; month <= 36; month += 12);}
-
-        //int amountPayment = service.calculate(amountCredit, month, percentYear);
-        //System.out.println("Кредит на 12 мес. Размер ежемесячного платежа: " + amountPayment + " рублей.");
-
-        System.out.println(service.calculate(amountCredit, month, percentYear));
-        System.out.println(service.calculate(1_000_000, 24, 9.99));
-        System.out.println(service.calculate(1_000_000, 36, 9.99));
+        System.out.println(service.calculate(percentYear, amountCredit, months));
+        System.out.println(service.calculate(9.99, 1_000_000, 24));
+        System.out.println(service.calculate(9.99, 1_000_000, 36));
     }
-
 }
